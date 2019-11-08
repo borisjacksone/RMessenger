e91d7fade3518c8bd483bf1c3089f482e242059f
Your Name <you@example.com>
2019-11-08 06:24:18 -0500

    Sixth Commit

--- app/src/main/assets/js/audio.js
+++ app/src/main/assets/js/audio.js
@@ -15,15 +15,26 @@ function startPlayer()
 
     player.addEventListener('canplaythrough', function() {
         var dur = Math.floor(player.duration);
-        var str = dur < 10 ? '0' + dur : dur;
+        var str = (dur < 10 ? '0' + dur : dur);
         document.getElementById('duration').innerHTML = str + 's';
 
         document.getElementById('loading_status').style.display = 'none';
         document.getElementById('duration').style.display = 'inline';
         document.getElementById('duration_label').style.display = 'inline';
         document.getElementById("play_button").style.display = 'inline';
-        document.getElementById("pause_button").style.display = 'inline';
+        document.getElementById("pause_button").style.display = 'none';
     }, false);
+
+    player.addEventListener('ended', function() {
+        player.currentTime = 0;
+        document.getElementById("play_button").style.display = 'inline';
+        document.getElementById("pause_button").style.display = 'none';
+    });
+
+    player.addEventListener('playing', function() {
+        document.getElementById("play_button").style.display = 'none';
+        document.getElementById("pause_button").style.display = 'inline';
+    });
 }
 
 function play_aud()
